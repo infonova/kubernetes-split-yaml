5e0fea680efc1d48524f3fe6224056d226ea6984
bhois <bernd.hois@bearingpoint.com>
2021-02-03 12:20:28 +0100

    Remove cleanYaml as it breaks some manifests

--- yaml.go
+++ yaml.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -10,15 +9,15 @@ import (
 func getYamlInfo(yamlContent string) (*KubernetesAPI, error) {
 
 	// Start by removing all lines with templating in to create sane yaml
-	cleanYaml := ""
-	for _, line := range strings.Split(yamlContent, "\n") {
-		if !strings.Contains(line, "{{") {
-			cleanYaml += line + "\n"
-		}
-	}
+	//cleanYaml := ""
+	//for _, line := range strings.Split(yamlContent, "\n") {
+	//	if !strings.Contains(line, "{{") {
+	//		cleanYaml += line + "\n"
+	//	}
+	//}
 
 	var m KubernetesAPI
-	err := yaml.Unmarshal([]byte(cleanYaml), &m)
+	err := yaml.Unmarshal([]byte(yamlContent), &m)
 	if err != nil {
 		return nil, fmt.Errorf("Could not unmarshal: %v \n---\n%v", err, yamlContent)
 	}
